d82c99488185bcbf1ade7f6c6c4d1b3bd3448742
Sean Lu <a05020312@gmail.com>
2025-02-21 16:40:57 +0800

    Initial upload | Implement and test core functions.

---
+++ internal/api/getLatestRecordList.go
@@ -0,0 +1,33 @@
+// as_qc_app/internal/api
+package api
+
+import (
+	"time"
+)
+
+type GetLatestRecordListRequest struct {
+	StartAt time.Time `json:"startAt"`
+	EndAt   time.Time `json:"endAt"`
+	Station string    `json:"station"` // If empty, send empty string, not null. Return all stations.
+}
+
+type GetLatestRecordListResponse struct {
+	Status         string           `json:"status"` // "success" or "error"
+	LatestDataList []LatestDataList `json:"latestDataList"`
+}
+
+type LatestDataList struct {
+	Station          string    `json:"station"`
+	TagId            string    `json:"tagId"`
+	Temperature      float64   `json:"temperature"`
+	Pressure         int       `json:"pressure"`
+	CableStatus      bool      `json:"cableStatus"`
+	TemperatureAlarm bool      `json:"temperatureAlarm"`
+	LowBatteryAlarm  bool      `json:"LowBatteryAlarm"`
+	BatteryLevel     int       `json:"batteryLevel"`
+	Timestamp        string    `json:"timestamp"`
+	FirmwareVersion  string    `json:"FirmwareVersion"`
+	TenMeterRssi     int       `json:"tenMeterRssi"`
+	Result           bool      `json:"result"`
+	ReceivedAt       time.Time `json:"receivedAt"`
+}
